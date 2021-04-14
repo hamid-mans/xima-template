0f74c7a01268c820fd54d20be0dc0b0813bbd53a
Hamid MANSOURI <mansouri.hamid23@gmail.com>
2021-04-14 17:08:03 +0200

    Template XIMA

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+    <head>
+        <title>Xima Template</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <body>
+        
+        <div class="banner">
+            <div class="titre"><p>XIMA</p></div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#realisations">Réalisations</a></li>
+                    <li><a href="#me">Me</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="layer"></div>
+
+        <div class="container">
+            <br><br><br><br>
+            <div class="presentation">
+                <span class="free">
+                    XIMA Template for you
+                </span><br><br>
+
+                XIMA Template is free and created for you with <i style="color: #ef2b2e;" class="fa fa-heart"></i><br><br>
+                Bonjour et bienvenue à vous. J'ai mis à disposition cette vitrine afin de vous montrer en perspective ma passion, ce qui me met en valeur ou encore le travail que je me démène à produire de jour à jour. Ce-même travail qui réunit passion et envies.
+                <br><br>
+                J'espère qu'avec ceci, vous découvrirez en détail qui est NOM PRENOM, en espérant également que mes diverses prestations vous en mêtent pleine la vue.
+            </div><br><br><br>
+
+        </div>
+
+
+        <div id="realisations"></div>
+        <div class="separator"><hr></div>
+
+        <div class="container">
+
+            <h1>Réalisations</h1>
+
+            <div class="products">
+
+                <div class="item">
+
+                </div>
+
+                <div class="item">
+                    
+                </div>
+
+                <div class="item">
+                    
+                </div>
+
+                <div class="item">
+                    
+                </div>
+
+                <div class="item">
+                    
+                </div>
+
+                <div class="item">
+                     
+                </div>
+            </div>
+
+        </div>
+
+        <div id="me"></div>
+        <div class="separator"><hr></div>
+
+        <div class="container">
+
+            <div class="me">
+                
+                <div class="picture">
+                    <img src="picture.png" alt="Photo">
+                </div>
+                
+                <div class="infos">
+                    <p class="name">NOM PRENOM</p>
+                    <p class="age">AGE ans</p>
+                    <p class="qualites">Je suis *qualité*, *qualité* et je dirai également *qualité*</p>
+
+                    <div class="parcours">
+                        <h3>Mon parcours</h3>
+
+                        <ul>
+                            <li>Brevet des collèges 2017-2018</li>
+                            <li>Formation technologique </li>
+                            <li>Baccalauréat technologique STMG</li>
+                        </ul>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div id="contact"></div>
+        <div class="separator"><hr></div>
+
+        <div class="container">
+            <h1>Me contacter</h1>
+            <br><br>
+            <div class="contact-bulle">
+            <form method="POST" class="contact">
+                <label for="prenom">Prénom</label><br>
+                <input type="text" name="prenom" id="prenom"><br><br>
+
+                <label for="nom">Nom</label><br>
+                <input type="text" name="nom" id="nom"><br><br>
+
+                <label for="demande">Demande</label><br>
+                <textarea name="demande" id="demande"></textarea><br><br>
+
+                <input type="submit" value="Envoyer">
+            </form>
+            </div>
+        </div><br><br><br>
+
+        <footer>
+            XIMA Template created with <i class="fa fa-heart"></i> by Hamid
+        </footer>
+
+        <script src="app.js"></script>
+    </body>
+</html>
